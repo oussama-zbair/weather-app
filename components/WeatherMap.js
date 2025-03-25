ec7ffef512fd4050baa921c7f224fde9a99da3b1
Oussama Zbair <oussama.zbair9@gmail.com>
2025-03-25 07:55:03 +0000

    changing the ui

--- components/WeatherMap.js
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const WeatherMapClient = dynamic(() => import('./WeatherMapClient'), { ssr: false });
-
-const WeatherMap = (props) => {
-  return <WeatherMapClient {...props} />;
-};
-
-export default WeatherMap;
--- pages/_app.js
+++
@@ -1,6 +0,0 @@
-import "@/styles/globals.css";
-import "mapbox-gl/dist/mapbox-gl.css";
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
